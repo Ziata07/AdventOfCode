e66b2283f729ffc5f91cb64c98389e5a1752e650
Mark James <mark.james0712@gmail.com>
2022-05-25 08:46:09 -0400

    Initial Code Commit

---
+++ AdventOfCode/AdventOfCodeFrontEnd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCodeFrontEnd
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//int zeroCheck;
+			//int oneCheck;			
+			string line;
+			StreamReader binaryNumFile = new StreamReader(@"F:\LearningProgramming\LearningCSharp\AdventOfCode_Day3\BinaryNumbers.txt");		
+
+			using (binaryNumFile)
+			{
+				for (int i = 0; i < 5; i++)
+				{//trying to split up the string, then get each char into it's own list
+					while ((line = binaryNumFile.ReadLine()) != null) //this line just goes through document
+					{
+						
+					}
+				}
+			}
+		}
+	}
+}
